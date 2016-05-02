95f14f58cd457a9aee3fd7a94fa3db84eb1a2192
Adam Stiff <stiff.4@osu.edu>
2016-05-02 11:35:02 -0400

    Initial commit
    App skeleton and ancillary project files

---
+++ source/app.d
@@ -0,0 +1,19 @@
+import std.stdio;
+import std.experimental.ndslice;
+
+void main()
+{
+  // execute with: tcbuilder <input_file> <output_file>
+  // parse input file
+  // place neurons
+  // calculate connection probabilities for each pair of neurons
+  // choose connections
+  // choose synapse locations for each connection
+  // calculate distances (presynaptic to synapse + synapse to postsynaptic)
+  // write network-level params
+  // write class-level params
+  // write neuron-level params
+  // write synapse-level params
+	
+}
+
